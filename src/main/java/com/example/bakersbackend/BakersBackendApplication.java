26e2ed02f65d8fff971d98be5a89f3e4b157fb30
woodgeon <ad4086@naver.com>
2025-11-17 00:19:15 +0900

    chore: 초기 프로젝트 구성

---
+++ src/main/java/com/example/bakersbackend/BakersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bakersbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakersBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakersBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bakersbackend/BakersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bakersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakersBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
