d034ae548f4cc049ba2b6917f6d0332b1ce0267c
woodgeon <ad4086@naver.com>
2025-11-17 12:11:09 +0900

    feat: 로그인 API 추가

--- src/main/java/com/example/bakersbackend/domain/auth/service/AuthService.java
+++ src/main/java/com/example/bakersbackend/domain/auth/service/AuthService.java
@@ -1,9 +1,13 @@
 package com.example.bakersbackend.domain.auth.service;
 
+import com.example.bakersbackend.domain.auth.dto.SignInRequest;
+import com.example.bakersbackend.domain.auth.dto.SignInResponse;
 import com.example.bakersbackend.domain.auth.dto.SignUpRequest;
 import com.example.bakersbackend.domain.auth.dto.SignUpResponse;
 import com.example.bakersbackend.domain.auth.entity.User;
 import com.example.bakersbackend.domain.auth.repository.UserRepository;
+import com.example.bakersbackend.global.jwt.JwtProvider;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +20,7 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
 
     public SignUpResponse signUp(SignUpRequest req) {
         // 사용 중인 이메일이 있다면
@@ -37,4 +42,22 @@ public class AuthService {
         return new SignUpResponse(savedUser.getId(), savedUser.getEmail(), savedUser.getNickname());
     }
 
+    public SignInResponse signIn(@Valid SignInRequest req) {
+
+        // 이메일로 유저 조회
+        User user = userRepository.findByEmail(req.email())
+                .orElseThrow(() -> new IllegalArgumentException("이메일 또는 비밀번호가 올바르지 않습니다."));
+        // 비미럽호 검증
+        if (!passwordEncoder.matches(req.password(), user.getPasswordHash())) {
+            throw new IllegalArgumentException("이메일 또는 비밀번호가 올바르지 않습니다.");
+        }
+        // 토큰 발급
+        String accessToken = jwtProvider.generateAccessToken(user);
+        String refreshToken = jwtProvider.generateRefreshToken(user);
+
+        // DB 저장
+        user.changeRefreshToken(refreshToken);
+
+        return new SignInResponse(accessToken, refreshToken, "Bearer", 1800);
+    }
 }
