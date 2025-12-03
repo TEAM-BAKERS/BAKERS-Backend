096e5c75077db99531524d7ae2431c63f645a0a9
woodgeon <ad4086@naver.com>
2025-12-03 15:08:41 +0900

    feat: S3 config 추가

---
+++ src/main/java/com/example/bakersbackend/global/config/S3Config.java
@@ -0,0 +1,26 @@
+package com.example.bakersbackend.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@RequiredArgsConstructor
+public class S3Config {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                // EC2 인스턴스의 IAM Role 자격증명을 자동으로 진행해줌
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
